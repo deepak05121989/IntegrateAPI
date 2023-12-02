998a6205f1473abde5b2a634be6d164faaa754f0
Acer <mailtodeepakchoudhary@gmail.com>
2023-12-02 13:45:41 +0530

    Add project files.

---
+++ IntegrateAPI/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace IntegrateAPI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new HttpClient();
+            var request = new HttpRequestMessage(HttpMethod.Get, "https://localhost:7202/WeatherForecast");
+            
+            request.Headers.Add("Authorization", "Basic dGVzdDp0ZXN0");
+            var response = client.SendAsync(request).GetAwaiter().GetResult();
+            response.EnsureSuccessStatusCode();
+            Console.WriteLine(response.Content.ReadAsStringAsync().GetAwaiter().GetResult());
+        }
+    }
+}
